3090ac3c847fe322c1a8ab674c733358791152f8
Aisezim <you@example.com>
2026-03-15 16:22:12 +0300

    3 students created

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        User student1 = new User(101,"Aisezim","Mederbekova",19);
+        User student2 = new User(102,"Aizirek","Amanova",19);
+        User student3 = new User(101,"Asema","isakova",20);
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+
+    }
 }
